706c26f8f6ce21b763a2f825a734d68387601f94
abilashchand <abilashmchand@gmail.com>
2023-08-01 07:11:29 +0530

    Algorithm Practice Commit

--- src/main/java/optionalPrograms/PractiseTest.java
+++ src/main/java/optionalPrograms/PractiseTest.java
@@ -3,6 +3,9 @@ package main.java.optionalPrograms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PractiseTest {
 
@@ -10,9 +13,33 @@ public class PractiseTest {
 
 		// System.out.println("Testing Main Method");
 
-		List<String> list = new ArrayList<String>(Arrays.asList("Bangalore", "Chennai", "Mumbai", "Delhi"));
-		String city = "Bangalore";
-		list.stream().forEach((a) -> System.out.println(a.contains(city)));
+		List<String> list = new ArrayList<String>(Arrays.asList("Alappuzha", "AGRA", "Bangalore", "Chennai", "Mumbai",
+				"Delhi", "Badrinath", "Bhubaneswar", "Bihar"));
+
+		list.stream().filter(s -> s.startsWith("A")).map(String::toUpperCase).forEach(System.out::println);
+
+		/*
+		 * System.out.println(
+		 * "-------------------------------------------------------------");
+		 * 
+		 * List<Integer> empIds = Arrays.asList(1, 2, 3, 4, 5);
+		 * 
+		 * list.stream().filter(s -> s.startsWith("B")).forEach(System.out::println);
+		 * 
+		 * System.out.println(
+		 * "-------------------------------------------------------------");
+		 * 
+		 * list.stream().sorted().map(String::toUpperCase).forEach(System.out::println);
+		 * 
+		 * System.out.println(
+		 * "-------------------------------------------------------------");
+		 * 
+		 * Stream<Integer> stream = empIds.stream(); // List<Integer> empIdlist =
+		 * stream.filter(i -> i * 10 == // 50).collect(Collectors.toList());
+		 * 
+		 * boolean empIdlist2 = stream.anyMatch(i -> i * 10 == 50);
+		 * System.out.println(empIdlist2);
+		 */
 
 	}
 
